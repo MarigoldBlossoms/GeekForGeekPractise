66974a95be3b980bb9c45d0c898cc8ef7bd33ec4
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-04-19 13:29:34 +1200

    C++ Data Type problem solved

---
+++ Basic/C++ Data Types/_src/datatype.cpp
@@ -0,0 +1,20 @@
+#include "datatype.h"
+
+int cppIntType() {
+    int i;
+    cin >> i;
+    return i;
+}
+
+char cppCharType() {
+    char c;
+    cin >> c;
+    return c;
+}
+
+    
+float cppFloatType() {
+    float f;
+    cin >> f;
+    return f;
+}
