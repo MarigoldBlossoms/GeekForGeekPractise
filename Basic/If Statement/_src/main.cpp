d77c9d6075b318042de51951838604ed0427c0ea
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-04-22 10:58:59 +1200

    If Statement problem completed

---
+++ Basic/If Statement/_src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int num;
+    cin >> num;
+    
+    if (num > 100) cout << "Big\n";
+    cout << "Number\n";
+    return 0;
+}
