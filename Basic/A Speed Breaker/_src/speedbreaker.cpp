1d95d0e6ff46f276db20b7cb0a4d0bcd17cffcea
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-04-23 13:50:45 +1200

    Speed Breaker problem solved

---
+++ Basic/A Speed Breaker/_src/speedbreaker.cpp
@@ -0,0 +1,5 @@
+#include "speedbreaker.h"
+
+void speedBreaker(double a, int b) {
+    printf("%.*f \n", b, a);
+}
---
+++ Basic/A Speed Breaker/_src/speedbreaker.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using namespace std;
+
+void speedBreaker(double a, int b);
