17f0a086ed54897ba4f6ad66cfd1170975950807
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-04-26 12:30:45 +1200

    C++ STL List problem completed

--- Basic/C++ STL Sets/_src/stl.h
+++ Basic/C++ STL Sets/_src/stl.h
@@ -3,6 +3,7 @@
 #include <stack>
 #include <map>
 #include <set>
+#include <list>
 
 using namespace std;
 
@@ -56,3 +57,26 @@ present in set s else returns -1 */
 int find(set<int> &s, int x);
 /*returns the size of the set s */
 int size(set<int> &s);
+
+//Lists
+/*prints space separated
+elements of list A*/
+void print(list<int> &A);
+/*remove element from
+back of list A*/
+void remove_from_back(list<int> &A);
+/*remove element from
+front of list A*/
+void remove_from_front(list<int> &A);
+/*inserts an element x at    
+the back of the list A */
+void add_to_list(list<int> &A, int x);
+/*sort the list A in ascending order*/
+void sort_list(list<int> &A);
+/*reverses the list A*/
+void reverse_list(list<int> &A);
+/*returns the size of the list  A */
+int size_of_list(list<int> &A);
+/*inserts an element x at
+the front of the list A*/
+void add_from_front(list<int> &A, int x);
