a4af5dfd64a04ecee6896a0d78075d3c7995d4bb
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-04-27 11:06:09 +1200

    Find +ve, -ve or 0 problem solved

---
+++ Basic/Find if +ve, -ve or 0/_src/checkNum.cpp
@@ -0,0 +1,16 @@
+#include "checkNum.h"
+
+void fun(int a) {
+    if (a > 0) 
+    {
+        cout << "Positive"; 
+    }
+    else if (a < 0)
+    { 
+        cout << "Negative"; 
+    }
+    else
+    {
+        cout << "Zero";
+    }
+}
