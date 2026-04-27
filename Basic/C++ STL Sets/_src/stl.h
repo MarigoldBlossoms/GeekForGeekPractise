94a254286173e1d3907f6e4738e1faa82d1d2fc9
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-04-27 16:17:15 +1200

    C++ STL Set Unordered Set problem fixed

--- Basic/C++ STL Sets/_src/stl.h
+++ Basic/C++ STL Sets/_src/stl.h
@@ -5,6 +5,7 @@
 #include <set>
 #include <list>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -122,3 +123,14 @@ int find_value(unordered_map<int, int> &m, int x);
 int getSize(unordered_map<int, int> &m);    
 /*Removes the entry with key x from the map */
 void removeKey(unordered_map<int, int> &m, int x);
+
+//Unordered set
+/*inserts an element x to the unordered set s */
+void insert(unordered_set<int> &s, int x);
+/*erases an element x from the unordered set s */
+void erase(unordered_set<int> &s, int x);
+/*returns the size of the unordered set s */
+int size(unordered_set<int> &s);
+/*returns 1 if the element x is present
+in unordered set s else returns -1 */
+int find(unordered_set<int> &s, int x);
---
+++ Basic/C++ STL Sets/_src/unorderedset.cpp
@@ -0,0 +1,24 @@
+#include "stl.h"
+
+/*inserts an element x to the unordered set s */
+void insert(unordered_set<int> &s, int x) {
+    s.insert(x);    
+}
+
+/*erases an element x from the unordered set s */
+void erase(unordered_set<int> &s, int x) {
+    if (s.empty()) return;
+    s.erase(x);
+}
+
+/*returns the size of the unordered set s */
+int size(unordered_set<int> &s) {
+    return s.size();
+}
+
+/*returns 1 if the element x is present
+in unordered set s else returns -1 */
+int find(unordered_set<int> &s, int x) {
+    if (s.find(x) != s.end()) return 1;
+    return -1;
+}
