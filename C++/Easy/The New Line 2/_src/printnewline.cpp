691a62e1a8150bd2aaf0f4c8526e1c5dde0bdf21
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-05-04 10:18:42 +1200

    The New Line 2 problem completed

---
+++ C++/Easy/The New Line 2/_src/printnewline.cpp
@@ -0,0 +1,13 @@
+#include "printnewline.h"
+
+void printString(string s)
+{
+    cout << s << endl;
+}
+
+void printNewLine() 
+{
+    printString("Geeks");
+    printString("for");
+    printString("Geeks");
+}
