6ba12165e323f2f284e78e83a13fee4605800daa
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-05-06 13:56:13 +1200

    All divisors of a number problem solved

---
+++ C++/Easy/All divisors of a number/_src/divisors.cpp
@@ -0,0 +1,21 @@
+#include "divisor.h"
+
+vector<int> getDivisors(int n) {
+    vector <int> divisors;
+
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+            
+            if (i != n / i)
+            {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+    
+    sort(divisors.begin(), divisors.end());
+    return divisors;    
+}
