3ae8d68ef16aece938e103ace36bad58a23e9847
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-05-10 12:19:46 +1200

    Learning Macros problem solved

---
+++ C++/Easy/Learning Macros/_src/product.cpp
@@ -0,0 +1,7 @@
+#include "product.h"
+
+void macros(int a, int b) {
+
+    cout << PRODUCT(a, b);
+    cout << endl;
+}
---
+++ C++/Easy/Learning Macros/_src/product.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+#define PRODUCT(a, b) (a * b)
+
+
+void macros(int a, int b);
