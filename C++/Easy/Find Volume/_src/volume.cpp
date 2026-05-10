d64715ef6cfd821d60f7eac73f3d366fed55f96f
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-05-11 11:26:34 +1200

    Find Volume problem solved

---
+++ C++/Easy/Find Volume/_src/volume.cpp
@@ -0,0 +1,11 @@
+// Function to calculate volume of a cube
+int volume_cube(int s) 
+{
+    return s*s*s;
+}
+
+// Function to calculate volume of a rectangular box
+int volume_box(int l, int w, int h)
+{
+    return l * w * h;
+}
