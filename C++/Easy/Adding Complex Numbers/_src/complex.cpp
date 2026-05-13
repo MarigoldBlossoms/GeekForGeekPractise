2b78e77d73aae927a879f67962f44c4f8957fec2
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-05-13 16:04:14 +1200

    Adding complex numbers problem solved

---
+++ C++/Easy/Adding Complex Numbers/_src/complex.cpp
@@ -0,0 +1,10 @@
+#include "complex.h"
+
+Complex add(Complex c1, Complex c2)
+{
+    Complex result;
+    result.real = c1.real + c2.real;
+    result.img = c1.img + c2.img;
+    
+    return result;
+}
---
+++ C++/Easy/Adding Complex Numbers/_src/complex.h
@@ -0,0 +1,12 @@
+#include <complex>
+
+using namespace std;
+
+struct Complex
+{
+    int real;
+    int img;
+};
+
+
+Complex add(Complex c1, Complex c2);
