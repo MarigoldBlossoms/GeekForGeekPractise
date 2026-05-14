14b37cbdf007246baed06d9f191bd9bc384ff780
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-05-14 17:04:27 +1200

    C++ Template problem solved

---
+++ C++/Easy/C++ Template/_src/minElement.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+class minElement
+{
+    private:
+        T y;
+
+    public:
+        minElement(T val)
+        {
+            y = val;
+        }
+
+        void check(T x)
+        {
+            if (x < y)
+            {
+                cout << x << endl;
+            }
+            else
+            {
+                cout << y << endl;
+            }
+        }
+};
