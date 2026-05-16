2410cf5d99dd1b809dd75822677461b3b900d584
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-05-17 09:44:42 +1200

    Scientific notation problem solved

---
+++ C++/Easy/Scientific Format/_src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double num;
+    cin >> num;
+
+    cout << scientific << setprecision(4) << num << endl; 
+    
+
+    return 0;
+}
